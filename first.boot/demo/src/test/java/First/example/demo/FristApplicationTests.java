059c293d89cb1946f56096ddfc14ccba835e6270
Alice Florencio <99298109+FlorenAlice@users.noreply.github.com>
2023-05-08 15:35:54 -0300

    Spring Web + React

---
+++ first.boot/demo/src/test/java/First/example/demo/FristApplicationTests.java
@@ -0,0 +1,13 @@
+package First.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FristApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
